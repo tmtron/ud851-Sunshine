5edb1bd98ff5308b58ef213fa8413a30d465ec0d
TMTron <martin.trummer@tmtron.com>
2017-11-11 15:33:42 +0100

    solved T03.02 RecyclerViewClickHandling

--- S03.02-Exercise-RecyclerViewClickHandling/app/src/main/java/com/example/android/sunshine/MainActivity.java
+++ S03.02-Exercise-RecyclerViewClickHandling/app/src/main/java/com/example/android/sunshine/MainActivity.java
@@ -26,6 +26,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.android.sunshine.data.SunshinePreferences;
 import com.example.android.sunshine.utilities.NetworkUtils;
@@ -33,12 +34,13 @@ import com.example.android.sunshine.utilities.OpenWeatherJsonUtils;
 
 import java.net.URL;
 
-// TODO (8) Implement ForecastAdapterOnClickHandler from the MainActivity
-public class MainActivity extends AppCompatActivity {
+// DONE (8) Implement ForecastAdapterOnClickHandler from the MainActivity
+public class MainActivity extends AppCompatActivity implements ForecastAdapter.ForecastAdapterOnClickHandler {
 
     private RecyclerView mRecyclerView;
     private ForecastAdapter mForecastAdapter;
 
+    private Toast mToast;
     private TextView mErrorMessageDisplay;
 
     private ProgressBar mLoadingIndicator;
@@ -73,12 +75,12 @@ public class MainActivity extends AppCompatActivity {
          */
         mRecyclerView.setHasFixedSize(true);
 
-        // TODO (11) Pass in 'this' as the ForecastAdapterOnClickHandler
+        // DONE (11) Pass in 'this' as the ForecastAdapterOnClickHandler
         /*
          * The ForecastAdapter is responsible for linking our weather data with the Views that
          * will end up displaying our weather data.
          */
-        mForecastAdapter = new ForecastAdapter();
+        mForecastAdapter = new ForecastAdapter(this);
 
         /* Setting the adapter attaches it to the RecyclerView in our layout. */
         mRecyclerView.setAdapter(mForecastAdapter);
@@ -107,9 +109,6 @@ public class MainActivity extends AppCompatActivity {
         new FetchWeatherTask().execute(location);
     }
 
-    // TODO (9) Override ForecastAdapterOnClickHandler's onClick method
-    // TODO (10) Show a Toast when an item is clicked, displaying that item's weather data
-
     /**
      * This method will make the View for the weather data visible and
      * hide the error message.
@@ -136,6 +135,15 @@ public class MainActivity extends AppCompatActivity {
         mRecyclerView.setVisibility(View.INVISIBLE);
         /* Then, show the error */
         mErrorMessageDisplay.setVisibility(View.VISIBLE);
+    }
+
+    // DONE (9) Override ForecastAdapterOnClickHandler's onClick method
+    @Override
+    public void onClick(String text) {
+        // DONE (10) Show a Toast when an item is clicked, displaying that item's weather data
+        if (mToast != null) mToast.cancel();
+        mToast = Toast.makeText(this, text, Toast.LENGTH_SHORT);
+        mToast.show();
     }
 
     public class FetchWeatherTask extends AsyncTask<String, Void, String[]> {
